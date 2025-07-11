81b55fd0982ba1a5f66ff1acd05df996a9a298bd
AbidShazid3 <abidshazid3@gmail.com>
2025-07-11 11:44:18 +0600

    part-1 completed

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
-import express, { Application } from "express";
-
+import express, { Application, Request, Response } from "express";
+import cors from 'cors';
+import { router } from "./app/routes";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
 
 const app: Application = express();
+app.use(express.json());
+app.use(cors())
 
-app.get('/', (req, res) => {
+app.use('/api/v1', router)
+
+app.get('/', (req: Request, res: Response) => {
     res.status(200).json({
         message: 'Hello World!, Welcome to Tour Management System Server'
     })
 })
+
+// global error handler
+app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, Request, Response } from "express";
+import httpStatus from 'http-status-codes';
+import { UserService } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+
+const createUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const user = await UserService.createUser(req.body)
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.CREATED,
+        message: 'User created successfully',
+        data: user,
+    })
+})
+
+
+const getAllUsers = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const result = await UserService.getAllUsers();
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: 'All users retrieved successfully',
+        data: result.data,
+        meta: result.meta
+    })
+})
+
+export const UserController = {
+    createUser,
+    getAllUsers
+}

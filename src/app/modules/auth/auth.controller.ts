21c0da310227925924bc2bfd9833132c5c9fbe48
AbidShazid3 <abidshazid3@gmail.com>
2025-07-16 10:27:58 +0600

    part-3 completed

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -4,11 +4,24 @@ import { catchAsync } from "../../utils/catchAsync"
 import sendResponse from "../../utils/sendResponse"
 import httpStatus from 'http-status-codes'
 import { AuthServices } from "./auth.service"
+import AppError from "../../errorHelpers/AppError"
+import { setAuthCookie } from "../../utils/setCookie"
+import { JwtPayload } from "jsonwebtoken"
+import { createUserToken } from "../../utils/userTokens"
+import { envVars } from "../../config/evn"
 
 const credentialsLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-
     const loginInfo = await AuthServices.credentialsLogin(req.body);
-    
+    setAuthCookie(res, loginInfo);
+    // res.cookie('accessToken', loginInfo.accessToken, {
+    //     httpOnly: true,
+    //     secure: false
+    // })
+    // res.cookie('refreshToken', loginInfo.refreshToken, {
+    //     httpOnly: true,
+    //     secure: false
+    // })
+
     sendResponse(res, {
         success: true,
         statusCode: httpStatus.OK,
@@ -17,6 +30,80 @@ const credentialsLogin = catchAsync(async (req: Request, res: Response, next: Ne
     })
 })
 
+const getNewAccessToken = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+        throw new AppError(httpStatus.BAD_REQUEST, 'No refresh token received from cookies')
+    }
+
+    const tokenInfo = await AuthServices.getNewAccessToken(refreshToken)
+
+    setAuthCookie(res, tokenInfo);
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: 'New access token retrieved successfully',
+        data: tokenInfo,
+    })
+})
+
+const logout = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+
+    res.clearCookie('accessToken', {
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax"
+    })
+    res.clearCookie('refreshToken', {
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax"
+    })
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: 'User logout successfully',
+        data: null,
+    })
+})
+
+const resetPassword = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const newPassword = req.body.newPassword;
+    const oldPassword = req.body.oldPassword;
+    const decodedToken = req.user;
+
+    await AuthServices.resetPassword(oldPassword, newPassword, decodedToken as JwtPayload)
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: 'Password changed successfully',
+        data: null,
+    })
+})
+
+const googleCallbackController = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    let redirectTo = req.query.state ? req.query.state as string : '';
+    if (redirectTo.startsWith('/')) {
+        redirectTo = redirectTo.slice(1)
+    }
+    const user = req.user;
+    if (!user) {
+        throw new AppError(httpStatus.NOT_FOUND, 'User not found')
+    }
+
+    const tokenInfo = createUserToken(user);
+    setAuthCookie(res, tokenInfo)
+
+    res.redirect(`${envVars.FRONTEND_URL}/${redirectTo}`)
+})
+
 export const AuthController = {
-    credentialsLogin
+    credentialsLogin,
+    getNewAccessToken,
+    logout,
+    resetPassword,
+    googleCallbackController,
 }
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import AppError from "../../errorHelpers/AppError";
 import { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
 import httpStatus from 'http-status-codes';
 import bcryptjs from 'bcryptjs';
-import { generateToken } from "../../utils/jwt";
+import { createNewAccessTokenWithRefreshToken, createUserToken } from "../../utils/userTokens";
+import { JwtPayload } from "jsonwebtoken";
 import { envVars } from "../../config/evn";
 
 const credentialsLogin = async (payload: Partial<IUser>) => {
@@ -19,22 +21,39 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
         throw new AppError(httpStatus.BAD_REQUEST, 'Incorrect Password')
     }
 
-    const jwtPayload = {
-        userId: isUserExist._id,
-        email: isUserExist.email,
-        role: isUserExist.role,
-    }
+    const userToken = createUserToken(isUserExist);
 
-    // const accessToken = jwt.sign(jwtPayload, 'secret', {
-    //     expiresIn: '1d'
-    // })
-    const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES)
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password: pass, ...rest } = isUserExist.toObject();
 
     return {
-       accessToken
+        accessToken: userToken.accessToken,
+        refreshToken: userToken.refreshToken,
+        user: rest,
     }
 }
 
+const getNewAccessToken = async (refreshToken: string) => {
+
+    const newAccessToken = await createNewAccessTokenWithRefreshToken(refreshToken);
+
+    return {
+        accessToken: newAccessToken
+    }
+}
+const resetPassword = async (oldPassword: string, newPassword: string, decodedToken: JwtPayload) => {
+    const user = await User.findById(decodedToken.userId)
+    const isOldPasswordMatch = await bcryptjs.compare(oldPassword, user!.password as string)
+    if (!isOldPasswordMatch) {
+        throw new AppError(httpStatus.UNAUTHORIZED, 'Old password does not match')
+    }
+
+    user!.password = await bcryptjs.hash(newPassword, Number(envVars.BCRYPT_SALT_ROUND))
+    user!.save();
+}
+
 export const AuthServices = {
-    credentialsLogin
+    credentialsLogin,
+    getNewAccessToken,
+    resetPassword,
 }

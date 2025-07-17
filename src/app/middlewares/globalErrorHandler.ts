75f74b9bf691bb0d831cf4e2a9cb92f16cd0024a
AbidShazid3 <abidshazid3@gmail.com>
2025-07-17 13:47:39 +0600

    in global error handler added duplicate error, castError and validation error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -5,11 +5,28 @@ import { envVars } from "../config/evn"
 import AppError from "../errorHelpers/AppError";
 
 export const globalErrorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
+    const errorSources: any = []
     let statusCode = 500;
     let message = 'Something went wrong'
 
-    if (error instanceof AppError) {
-        statusCode = error.statusCode
+    if (error.code === 11000) {
+        const matchArray = error.message.match(/"([^"]*)"/)
+        statusCode = 400;
+        message = `${matchArray[1]} already exists`;
+    } else if (error.name === 'CastError') {
+        statusCode = 400;
+        message = 'Invalid mongoDB object id. Pls provide a valid id'
+    } else if (error.name === 'ValidationError') {
+        statusCode = 400;
+        const errors = Object.values(error.errors)
+        
+        errors.forEach((errorObject: any) => errorSources.push({
+            path: errorObject.path,
+            message: errorObject.message
+        }))
+        message = 'Validation error occurred'
+    } else if (error instanceof AppError) {
+        statusCode = error.statusCode;
         message = error.message
     } else if (error instanceof Error) {
         statusCode = 500
@@ -19,7 +36,8 @@ export const globalErrorHandler = (error: any, req: Request, res: Response, next
     res.status(statusCode).json({
         success: false,
         message,
-        error,
+        errorSources,
+        // error,
         stack: envVars.NODE_ENV === 'development' ? error.stack : null
     })
 }

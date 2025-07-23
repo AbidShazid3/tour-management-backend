a1da327f5e6a841b7704341f7499eddfa065f397
AbidShazid3 <abidshazid3@gmail.com>
2025-07-23 21:00:17 +0600

    ssl payment done

--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import AppError from "../../errorHelpers/AppError";
 import { User } from "../user/user.model";
 import { BookingStatus, IBooking } from "./booking.interface";
@@ -6,6 +7,8 @@ import { Booking } from "./booking.model";
 import { Payment } from "../payment/payment.model";
 import { PaymentStatus } from "../payment/payment.interface";
 import { Tour } from "../tour/tour.model";
+import { SSLService } from "../sslCommerz/sslCommerz.service";
+import { ISSLCommerz } from "../sslCommerz/sslCommerz.interface";
 
 const getTransactionId = () => {
     return `tran_${Date.now()}_${Math.floor(Math.random() * 1000)}`
@@ -56,9 +59,27 @@ const createBooking = async (payload: Partial<IBooking>, userId: string) => {
             .populate('tour', 'title costFrom')
             .populate('payment');
 
+        const userAddress = (updatedBooking?.user as any).address;
+        const userEmail = (updatedBooking?.user as any).email;
+        const userPhone = (updatedBooking?.user as any).phone;
+        const userName = (updatedBooking?.user as any).name;
+        const sslPayload: ISSLCommerz = {
+            address: userAddress,
+            email: userEmail,
+            phoneNumber: userPhone,
+            name: userName,
+            amount: amount,
+            transactionId: transactionId
+        }
+        const sslPayment = await SSLService.sslPaymentInit(sslPayload)
+        console.log(sslPayment);
+
         await session.commitTransaction();
 
-        return updatedBooking;
+        return {
+            paymentUrl: sslPayment.GatewayPageURL,
+            booking: updatedBooking,
+        };
     } catch (error) {
         await session.abortTransaction();
 
